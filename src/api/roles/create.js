c2df58a482c5c8f611493d46b30867d3705f510c
Brun0Silveir4 <bruno.silveira.dionisio@gmail.com>
2025-11-14 15:48:36 -0300

    new: add role route

--- src/api/roles/create.js
+++ src/api/roles/create.js
@@ -1,5 +1,28 @@
-const prisma = require('../database/index.js')
+const prisma = require("../database/index.js");
+const { randomUUID } = require("crypto");
 
-async function createRole(name){
-    const create = await
-}
+const createRole = async (req, res) => {
+  if (!req.body || !req.body.roleName) {
+    return res.status(400).json({ message: "roleName is a mandatory field." });
+  }
+  const { roleName } = req.body;
+
+  try {
+    const result = await prisma.role.create({
+      data: {
+        roleName,
+        roleId: randomUUID(),
+      },
+    });
+    return res.status(200).json(result);
+  } catch (e) {
+    if (e.code == "P2002") {
+      return res
+        .status(400)
+        .json({ message: "A role with this name already exists" });
+    }
+    return res.status(400).json({ error: e.message });
+  }
+};
+
+module.exports = createRole;
--- src/routes/Router.js
+++ src/routes/Router.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const router = express.Router()
+const roles = require('../api/roles/index')
 
+
+router.post('/roles', roles.create)
 
 
 module.exports = router
